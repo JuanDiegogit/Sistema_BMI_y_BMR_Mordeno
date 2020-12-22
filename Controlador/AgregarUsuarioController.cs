2d6a229a58fc27715de2f901ad75484cf304cb60
Juan Diego Mejía Maestre <65135568+JuanDiegogit@users.noreply.github.com>
2020-12-22 17:38:53 -0500

    Agregar Clases controladoras

---
+++ Controlador/AgregarUsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Modelo;
+using System.Data.Entity;
+
+namespace Controlador
+{
+   public class AgregarUsuarioController
+    {
+
+        public async Task<int> GuardarUsuarioAsync(USUARIO Usuario)
+        {
+            try
+            {
+
+                using (SISTEMA_BMIEntities modelo = new SISTEMA_BMIEntities())
+                {
+
+                    modelo.USUARIO.Add(Usuario);
+                    return await modelo.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+
+        }
+    }
+}
---
+++ Controlador/ConsultarUsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using Modelo.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class ConsultarUsuarioController
+    {
+        public async Task<List<USUARIO>> ObtenerUsuarios()
+        {
+            try
+            {
+
+                using (SISTEMA_BMIEntities modelo = new SISTEMA_BMIEntities())
+                {
+                    return await modelo.USUARIO.ToListAsync();
+                }
+            }
+            catch (Exception)
+            {
+                return default(List<USUARIO>);
+            }
+
+
+        }
+    }
+}
