4fbc6d001c0c88164fa4649e76809ad0b46f2dd4
Juan Diego Mejía Maestre <65135568+JuanDiegogit@users.noreply.github.com>
2020-12-22 14:09:16 -0500

    Eliminar capas modelo y controlador

--- Vista/AgregarUsuario.Designer.cs
+++ Vista/AgregarUsuario.Designer.cs
@@ -33,10 +33,18 @@
             this.btnCerrar = new System.Windows.Forms.PictureBox();
             this.btnMaximizarRestaurar = new System.Windows.Forms.PictureBox();
             this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelPrincipalContenedor = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.panelSuperior.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMaximizarRestaurar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            this.panelPrincipal.SuspendLayout();
+            this.panelPrincipalContenedor.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelSuperior
@@ -86,12 +94,76 @@
             this.btnMinimizar.TabStop = false;
             this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
             // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.Controls.Add(this.panelPrincipalContenedor);
+            this.panelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPrincipal.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panelPrincipal.Location = new System.Drawing.Point(0, 28);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Padding = new System.Windows.Forms.Padding(20, 10, 20, 10);
+            this.panelPrincipal.Size = new System.Drawing.Size(800, 422);
+            this.panelPrincipal.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // panelPrincipalContenedor
+            // 
+            this.panelPrincipalContenedor.Controls.Add(this.textBox1);
+            this.panelPrincipalContenedor.Controls.Add(this.label1);
+            this.panelPrincipalContenedor.Controls.Add(this.shapeContainer1);
+            this.panelPrincipalContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPrincipalContenedor.Location = new System.Drawing.Point(20, 10);
+            this.panelPrincipalContenedor.Name = "panelPrincipalContenedor";
+            this.panelPrincipalContenedor.Size = new System.Drawing.Size(760, 402);
+            this.panelPrincipalContenedor.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(21, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(712, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(760, 402);
+            this.shapeContainer1.TabIndex = 2;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lineShape1.BorderWidth = 2;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 19;
+            this.lineShape1.X2 = 734;
+            this.lineShape1.Y1 = 78;
+            this.lineShape1.Y2 = 78;
+            // 
             // AgregarUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelPrincipal);
             this.Controls.Add(this.panelSuperior);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -103,6 +175,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMaximizarRestaurar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            this.panelPrincipal.ResumeLayout(false);
+            this.panelPrincipalContenedor.ResumeLayout(false);
+            this.panelPrincipalContenedor.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -113,5 +188,11 @@
         private System.Windows.Forms.PictureBox btnCerrar;
         private System.Windows.Forms.PictureBox btnMaximizarRestaurar;
         private System.Windows.Forms.PictureBox btnMinimizar;
+        private System.Windows.Forms.Panel panelPrincipal;
+        private System.Windows.Forms.Panel panelPrincipalContenedor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
     }
 }
